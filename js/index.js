29c4d5dfe315ba1a3c6c8fc35d2526a3be6b796a
BenjaminBLi <li.benjamin.b@gmail.com>
2019-01-25 16:18:04 -0500

    started working on jquery to query github api

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-$( document ).ready( function() {
-    $.ajax()
-}); //close document ready
+(function($){
+    $(document).ready(function(){
+        $("#test-info").mouseenter(function(){
+            var test = "";
+            $("#test-info").html=("");
+            $.ajax({
+                url : "https://api.github.com/users/BenjaminBLi/followers",
+                dataType : "jsonp",
+                
+            });
+        });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space

322dff23210b2a4c7d9e00482f9815dac8f581e5
BenjaminBLi <li.benjamin.b@gmail.com>
2019-01-29 13:56:43 -0500

    started working on jquery to query github api

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
 (function($){
     $(document).ready(function(){
-        $("#test-info").mouseenter(function(){
-            var test = "";
-            $("#test-info").html=("");
+        $("#submit").on('click', function(e){
+            e.preventDefault();
+
+            $('#usernames').html("loading...");
+
+            let user = $("#userid").val();
+            console.log(user);
+
+            let usernames = [];
             $.ajax({
-                url : "https://api.github.com/users/BenjaminBLi/followers",
+                url : "https://api.github.com/users/" + user + "/followers",
                 dataType : "jsonp",
                 
+            }).done(function(json){
+                //if username is queried successfully, find
+                //console.log( json.data);
+                for(let i = 0; i < json.data.length; i++) {
+                    usernames.push(json.data[i].login);
+                }
+                console.log(usernames);
+                $("#usernames").html("<code>" + usernames + "</code>")
             });
+
+            //let's avoid the pyramid of doom shall we
+            //after getting the usernames, I want to sum up the amount of code contributed in the past week by username
+
+            for(let i = 0; i < usernames.length; i++){
+                //get list of repos
+
+                //get code freq per repo and sum together
+            }
+
+
         });
 
     }); // end of document ready

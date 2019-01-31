4c3fdd38ae816f01efe3c60c03bf1fa5f13d099a
Ben <li.benjamin.b@gmail.com>
2019-01-30 23:19:01 -0500

    worked on jquery code to properly display sorted users based on code frequency of all repos. Didn't finish.

--- js/index.js
+++ js/index.js
@@ -19,21 +19,45 @@
                 for(let i = 0; i < json.data.length; i++) {
                     usernames.push(json.data[i].login);
                 }
-                console.log(usernames);
-                $("#usernames").html("<code>" + usernames + "</code>")
+                //console.log(usernames);
+
+                for(let i = 0; i < usernames.length; i++){
+                    //get list of repos
+                    let curr_user = usernames[i];
+                    let repos = [];
+
+                    $.getJSON("https://api.github.com/users/"+curr_user+"/repos", function(data){
+                        $.each(data, function(key, val){
+                            repos.push(val.name);
+                        });
+                    });
+                    console.log(curr_user);
+                    console.log(repos);
+                    //get code freq per repo and sum together
+                    let user_sum = 0;
+                    /*
+                    for (let i = 0; i < repos.length; i++){
+                        $.getJSON("https://api.github.com/repos/"+curr_user+"/"+repos[i]+"/stats/code_frequency",
+                            function(data){
+                                console.log(data);
+                            });
+                    }*/
+                    console.log(user_sum);
+                }
+
+                $("#usernames").html(usernames_as_html(usernames));
             });
-
-            //let's avoid the pyramid of doom shall we
-            //after getting the usernames, I want to sum up the amount of code contributed in the past week by username
-
-            for(let i = 0; i < usernames.length; i++){
-                //get list of repos
-
-                //get code freq per repo and sum together
-            }
-
-
         });
-
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function usernames_as_html(usernames){
+    let ret = "<ul>\n";
+    for(let i = 0; i < usernames.length; i++){
+        ret += "<li> ";
+        ret += usernames[i];
+        ret += "</li>\n";
+    }
+    ret += "</ul>\n";
+    return ret;
+}
